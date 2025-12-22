cec04532ec7887b712debd1dcf057aa0f8d1159b
Rahul N Bangera <bn2345890@gmail.com>
2025-12-22 23:47:51 +0530

    feat: added flc point easteregg

---
+++ src/app/api/easteregg/[action]/route.ts
@@ -0,0 +1,96 @@
+import { type NextRequest, NextResponse } from "next/server";
+import { db } from "~/server/db";
+import { parseJwtFromAuthHeader } from "~/lib/utils";
+
+export async function POST(req: NextRequest) {
+	try {
+		const url = req.nextUrl;
+		const action = url.pathname.split("/").pop();
+
+		let body: unknown = {};
+
+		switch (action) {
+			case "giveflceasterpoints": {
+				const { easterEggId } = body as { easterEggId: number };
+				const customHeader = req.headers.get("authorization");
+				const data = parseJwtFromAuthHeader(customHeader || "");
+				if (!data || !data.userId) {
+					return NextResponse.json(
+						{ success: false, error: "Invalid or missing authentication data" },
+						{ status: 401 },
+					);
+				}
+
+				const user = await db.user.findUnique({
+					where: { id: data.userId },
+					include: { easterEgg: true },
+				});
+
+				if (!user) {
+					return NextResponse.json(
+						{ success: false, error: "User not found" },
+						{ status: 404 },
+					);
+				}
+
+				const easterEgg = await db.easterEgg.findUnique({
+					where: { id: easterEggId },
+				});
+
+				if (!easterEgg) {
+					return NextResponse.json(
+						{ success: false, error: "Easter egg not found" },
+						{ status: 404 },
+					);
+				}
+
+				const existingEgg = await db.user.findFirst({
+					where: {
+						id: data.userId,
+						easterEggId: easterEggId,
+					},
+				});
+
+				if (existingEgg) {
+					return NextResponse.json(
+						{ success: false, error: "Easter egg already claimed" },
+						{ status: 204 },
+					);
+				}
+
+				const updatedUser = await db.user.update({
+					where: { id: data.userId },
+					data: {
+						flcPoints: { increment: easterEgg.flcPoints },
+						easterEggId: easterEggId,
+					},
+					select: {
+						totalActivityPoints: true,
+					},
+				});
+
+				const result = {
+					success: true,
+					message: `Awarded ${easterEgg.flcPoints} FLC points for finding the easter egg!`,
+					totalFLCPoints: updatedUser.totalActivityPoints,
+				};
+
+				return NextResponse.json(result, {
+					status: result.success ? 200 : 400,
+				});
+			}
+
+			default:
+				return NextResponse.json(
+					{ success: false, error: "Unknown action" },
+					{ status: 400 },
+				);
+		}
+	} catch (error) {
+		console.error("Public Events API Error:", error);
+		return NextResponse.json(
+			{ success: false, error: (error as Error).message },
+			{ status: 500 },
+		);
+	}
+}
