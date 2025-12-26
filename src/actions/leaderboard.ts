6beebbe050d0a4ccd0d5f1c8d412780db1fc57fa
Rahul N Bangera <bn2345890@gmail.com>
2025-12-26 18:13:32 +0530

    fix: core team exclusion from leaderboard

--- src/actions/leaderboard.ts
+++ src/actions/leaderboard.ts
@@ -1,9 +1,21 @@
 "use server";
 import { db } from "~/server/db";
 
+const EXCLUDED_USER_IDS = [
+	1, 414, 289, 181, 13, 478, 482, 454, 500, 531, 393, 453, 583, 9, 205, 74, 18,
+	57, 940, 1114, 418, 69, 1200, 1188, 46, 953, 8, 366, 546, 37, 359, 177, 166,
+	995, 139, 104, 870, 17, 222, 2754, 141, 2326, 1190, 868, 65, 1103, 2044, 515,
+	4, 388, 2948, 158,
+];
+
 export async function getLeaderboardData() {
 	try {
 		const leaderboardRaw = await db.user.findMany({
+			where: {
+				id: {
+					notIn: EXCLUDED_USER_IDS,
+				},
+			},
 			orderBy: [{ totalActivityPoints: "desc" }, { id: "asc" }],
 			select: {
 				id: true,
@@ -50,6 +62,9 @@ export async function getRankOfUser(userId: number) {
 			where: {
 				totalActivityPoints: {
 					gt: user.totalActivityPoints,
+				},
+				id: {
+					notIn: EXCLUDED_USER_IDS,
 				},
 			},
 		});
